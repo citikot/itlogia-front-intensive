d99df498616ccb46240d801c17d4ef928249b329
Alexander Pishvanov <apishvanov@gmail.com>
2022-09-25 13:19:51 +0300

    module5 hw done

--- cars-app/src/app/app.component.ts
+++ cars-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,87 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goScroll(target: HTMLElement) {
+
+  priceForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      car: ['', Validators.required],
+    }
+  );
+
+  carsData = [
+    {
+      image: "1.png",
+      name: "Lamborghini Huracan Spyder",
+      gear: "Полный",
+      engine: 5.2,
+      places: 2
+    },
+    {
+      image: "2.png",
+      name: "Chevrolet Corvette",
+      gear: "Полный",
+      engine: 6.2,
+      places: 2
+    },
+    {
+      image: "3.png",
+      name: "Ferrari California",
+      gear: "Полный",
+      engine: 3.9,
+      places: 4
+    },
+    {
+      image: "4.png",
+      name: "Lamborghini Urus",
+      gear: "Полный",
+      engine: 4,
+      places: 5
+    },
+    {
+      image: "5.png",
+      name: "Audi R8",
+      gear: "Полный",
+      engine: 5.2,
+      places: 2
+    },
+    {
+      image: "6.png",
+      name: "Аренда Chevrolet Camaro",
+      gear: "Задний",
+      engine: 2,
+      places: 4
+    }
+  ];
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement, car?: any) {
     target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
+    }
+  }
+
+  trans: any;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert("Спасибо за заявку!");
+      this.priceForm.reset();
+    }
   }
 }
